bfe6df0cb2ac55437fdbac976a2ae7ff4071633a
Raphael Adamski <raphaeladamski@hotmail.com>
2018-11-10 19:18:38 -0200

    use an output path for images

--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-bin/iftSegmentObject data/bird.png data/bird.txt 0.7 res-bird.png
-bin/iftSegmentObject data/cat.png data/cat.txt 0.8 res-cat.png
-bin/iftSegmentObject data/dog.png data/dog.txt 0.7 res-dog.png
-bin/iftSegmentObject data/flowers.png data/flowers.txt 0.1 res-flowers.png
-bin/iftSegmentObject data/horses.png data/horses.txt 0.9 res-horses.png
-bin/iftSegmentObject data/melanoma.png data/melanoma.txt 0.9 res-melanoma.png
-bin/iftSegmentObject data/parasite.png data/parasite.txt 0.5 res-parasite.png
-bin/iftSegmentObject data/rocks.png data/rocks.txt 0.9 res-rocks.png
-bin/iftSegmentObject data/surfer.png data/surfer.txt 0.8 res-surfer.png
-bin/iftSegmentObject data/teddy.png data/teddy.txt 0.7 res-teddy.png
-bin/iftSegmentObject data/liver.png data/liver.txt 0.9 res-liver.png
+bin/iftSegmentObject data/bird.png data/bird.txt 0.7 out/res-bird.png
+bin/iftSegmentObject data/cat.png data/cat.txt 0.8 out/res-cat.png
+bin/iftSegmentObject data/dog.png data/dog.txt 0.7 out/res-dog.png
+bin/iftSegmentObject data/flowers.png data/flowers.txt 0.1 out/res-flowers.png
+bin/iftSegmentObject data/horses.png data/horses.txt 0.9 out/res-horses.png
+bin/iftSegmentObject data/melanoma.png data/melanoma.txt 0.9 out/res-melanoma.png
+bin/iftSegmentObject data/parasite.png data/parasite.txt 0.5 out/res-parasite.png
+bin/iftSegmentObject data/rocks.png data/rocks.txt 0.9 out/res-rocks.png
+bin/iftSegmentObject data/surfer.png data/surfer.txt 0.8 out/res-surfer.png
+bin/iftSegmentObject data/teddy.png data/teddy.txt 0.7 out/res-teddy.png
+bin/iftSegmentObject data/liver.png data/liver.txt 0.9 out/res-liver.png
