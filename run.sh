564fb0f3a3699d8c372e1b37fa389681b1f385ca
Raphael Adamski <raphaeladamski@hotmail.com>
2018-11-11 13:00:17 -0200

    echo each command

--- run.sh
+++ run.sh
@@ -1,12 +1,23 @@
 #!/usr/bin/env bash
+echo bin/iftSegmentObject data/bird.png data/bird.txt 0.7 out/res-bird.png
 bin/iftSegmentObject data/bird.png data/bird.txt 0.7 out/res-bird.png
+echo bin/iftSegmentObject data/cat.png data/cat.txt 0.8 out/res-cat.png
 bin/iftSegmentObject data/cat.png data/cat.txt 0.8 out/res-cat.png
+echo bin/iftSegmentObject data/dog.png data/dog.txt 0.7 out/res-dog.png
 bin/iftSegmentObject data/dog.png data/dog.txt 0.7 out/res-dog.png
+echo bin/iftSegmentObject data/flowers.png data/flowers.txt 0.1 out/res-flowers.png
 bin/iftSegmentObject data/flowers.png data/flowers.txt 0.1 out/res-flowers.png
+echo bin/iftSegmentObject data/horses.png data/horses.txt 0.9 out/res-horses.png
 bin/iftSegmentObject data/horses.png data/horses.txt 0.9 out/res-horses.png
+echo bin/iftSegmentObject data/melanoma.png data/melanoma.txt 0.9 out/res-melanoma.png
 bin/iftSegmentObject data/melanoma.png data/melanoma.txt 0.9 out/res-melanoma.png
+echo bin/iftSegmentObject data/parasite.png data/parasite.txt 0.5 out/res-parasite.png
 bin/iftSegmentObject data/parasite.png data/parasite.txt 0.5 out/res-parasite.png
+echo bin/iftSegmentObject data/rocks.png data/rocks.txt 0.9 out/res-rocks.png
 bin/iftSegmentObject data/rocks.png data/rocks.txt 0.9 out/res-rocks.png
+echo bin/iftSegmentObject data/surfer.png data/surfer.txt 0.8 out/res-surfer.png
 bin/iftSegmentObject data/surfer.png data/surfer.txt 0.8 out/res-surfer.png
+echo bin/iftSegmentObject data/teddy.png data/teddy.txt 0.7 out/res-teddy.png
 bin/iftSegmentObject data/teddy.png data/teddy.txt 0.7 out/res-teddy.png
+echo bin/iftSegmentObject data/liver.png data/liver.txt 0.9 out/res-liver.png
 bin/iftSegmentObject data/liver.png data/liver.txt 0.9 out/res-liver.png
