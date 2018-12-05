4ab49ec1b8f54c49af3304e4207fc4838ebebc1a
Raphael Adamski <raphaeladamski@hotmail.com>
2018-12-05 00:47:47 -0200

    Update runnable to generate all images from all algorithms implemented

--- run.sh
+++ run.sh
@@ -1,23 +1,144 @@
 #!/usr/bin/env bash
-echo bin/iftSegmentObject data/bird.png data/bird.txt 0.7 out/res-bird.png
-bin/iftSegmentObject data/bird.png data/bird.txt 0.7 out/res-bird.png
-echo bin/iftSegmentObject data/cat.png data/cat.txt 0.8 out/res-cat.png
-bin/iftSegmentObject data/cat.png data/cat.txt 0.8 out/res-cat.png
-echo bin/iftSegmentObject data/dog.png data/dog.txt 0.7 out/res-dog.png
-bin/iftSegmentObject data/dog.png data/dog.txt 0.7 out/res-dog.png
-echo bin/iftSegmentObject data/flowers.png data/flowers.txt 0.1 out/res-flowers.png
-bin/iftSegmentObject data/flowers.png data/flowers.txt 0.1 out/res-flowers.png
-echo bin/iftSegmentObject data/horses.png data/horses.txt 0.9 out/res-horses.png
-bin/iftSegmentObject data/horses.png data/horses.txt 0.9 out/res-horses.png
-echo bin/iftSegmentObject data/melanoma.png data/melanoma.txt 0.9 out/res-melanoma.png
-bin/iftSegmentObject data/melanoma.png data/melanoma.txt 0.9 out/res-melanoma.png
-echo bin/iftSegmentObject data/parasite.png data/parasite.txt 0.5 out/res-parasite.png
-bin/iftSegmentObject data/parasite.png data/parasite.txt 0.5 out/res-parasite.png
-echo bin/iftSegmentObject data/rocks.png data/rocks.txt 0.9 out/res-rocks.png
-bin/iftSegmentObject data/rocks.png data/rocks.txt 0.9 out/res-rocks.png
-echo bin/iftSegmentObject data/surfer.png data/surfer.txt 0.8 out/res-surfer.png
-bin/iftSegmentObject data/surfer.png data/surfer.txt 0.8 out/res-surfer.png
-echo bin/iftSegmentObject data/teddy.png data/teddy.txt 0.7 out/res-teddy.png
-bin/iftSegmentObject data/teddy.png data/teddy.txt 0.7 out/res-teddy.png
-echo bin/iftSegmentObject data/liver.png data/liver.txt 0.9 out/res-liver.png
-bin/iftSegmentObject data/liver.png data/liver.txt 0.9 out/res-liver.png
+echo region
+echo bin/iftSegmentObject data/bird.png data/bird.txt 0.7 out/res-region-bird.png --region
+bin/iftSegmentObject data/bird.png data/bird.txt 0.7 out/res-bird-region.png --region
+echo bin/iftSegmentObject data/cat.png data/cat.txt 0.8 out/res-cat-region.png --region
+bin/iftSegmentObject data/cat.png data/cat.txt 0.8 out/res-cat-region.png --region
+echo bin/iftSegmentObject data/dog.png data/dog.txt 0.7 out/res-dog-region.png --region
+bin/iftSegmentObject data/dog.png data/dog.txt 0.7 out/res-dog-region.png --region
+echo bin/iftSegmentObject data/flowers.png data/flowers.txt 0.1 out/res-flowers-region.png --region
+bin/iftSegmentObject data/flowers.png data/flowers.txt 0.1 out/res-flowers-region.png --region
+echo bin/iftSegmentObject data/horses.png data/horses.txt 0.9 out/res-horses-region.png --region
+bin/iftSegmentObject data/horses.png data/horses.txt 0.9 out/res-horses-region.png --region
+echo bin/iftSegmentObject data/melanoma.png data/melanoma.txt 0.9 out/res-melanoma-region.png --region
+bin/iftSegmentObject data/melanoma.png data/melanoma.txt 0.9 out/res-melanoma-region.png --region
+echo bin/iftSegmentObject data/parasite.png data/parasite.txt 0.5 out/res-parasite-region.png --region
+bin/iftSegmentObject data/parasite.png data/parasite.txt 0.5 out/res-parasite-region.png --region
+echo bin/iftSegmentObject data/rocks.png data/rocks.txt 0.9 out/res-rocks-region.png --region
+bin/iftSegmentObject data/rocks.png data/rocks.txt 0.9 out/res-rocks-region.png --region
+echo bin/iftSegmentObject data/surfer.png data/surfer.txt 0.8 out/res-surfer-region.png --region
+bin/iftSegmentObject data/surfer.png data/surfer.txt 0.8 out/res-surfer-region.png --region
+echo bin/iftSegmentObject data/teddy.png data/teddy.txt 0.7 out/res-teddy-region.png --region
+bin/iftSegmentObject data/teddy.png data/teddy.txt 0.7 out/res-teddy-region.png --region
+echo bin/iftSegmentObject data/liver.png data/liver.txt 0.9 out/res-liver-region.png --region
+bin/iftSegmentObject data/liver.png data/liver.txt 0.9 out/res-liver-region.png --region
+
+echo gradient
+echo bin/iftSegmentObject data/bird.png data/bird.txt 0.7 out/res-bird-gradient.png --gradient
+bin/iftSegmentObject data/bird.png data/bird.txt 0.7 out/res-bird-gradient.png --gradient
+echo bin/iftSegmentObject data/cat.png data/cat.txt 0.8 out/res-cat-gradient.png --gradient
+bin/iftSegmentObject data/cat.png data/cat.txt 0.8 out/res-cat-gradient.png --gradient
+echo bin/iftSegmentObject data/dog.png data/dog.txt 0.7 out/res-dog-gradient.png --gradient
+bin/iftSegmentObject data/dog.png data/dog.txt 0.7 out/res-dog-gradient.png --gradient
+echo bin/iftSegmentObject data/flowers.png data/flowers.txt 0.1 out/res-flowers-gradient.png --gradient
+bin/iftSegmentObject data/flowers.png data/flowers.txt 0.1 out/res-flowers-gradient.png --gradient
+echo bin/iftSegmentObject data/horses.png data/horses.txt 0.9 out/res-horses-gradient.png --gradient
+bin/iftSegmentObject data/horses.png data/horses.txt 0.9 out/res-horses-gradient.png --gradient
+echo bin/iftSegmentObject data/melanoma.png data/melanoma.txt 0.9 out/res-melanoma-gradient.png --gradient
+bin/iftSegmentObject data/melanoma.png data/melanoma.txt 0.9 out/res-melanoma-gradient.png --gradient
+echo bin/iftSegmentObject data/parasite.png data/parasite.txt 0.5 out/res-parasite-gradient.png --gradient
+bin/iftSegmentObject data/parasite.png data/parasite.txt 0.5 out/res-parasite-gradient.png --gradient
+echo bin/iftSegmentObject data/rocks.png data/rocks.txt 0.9 out/res-rocks-gradient.png --gradient
+bin/iftSegmentObject data/rocks.png data/rocks.txt 0.9 out/res-rocks-gradient.png --gradient
+echo bin/iftSegmentObject data/surfer.png data/surfer.txt 0.8 out/res-surfer-gradient.png --gradient
+bin/iftSegmentObject data/surfer.png data/surfer.txt 0.8 out/res-surfer-gradient.png --gradient
+echo bin/iftSegmentObject data/teddy.png data/teddy.txt 0.7 out/res-teddy-gradient.png --gradient
+bin/iftSegmentObject data/teddy.png data/teddy.txt 0.7 out/res-teddy-gradient.png --gradient
+echo bin/iftSegmentObject data/liver.png data/liver.txt 0.9 out/res-liver-gradient.png --gradient
+bin/iftSegmentObject data/liver.png data/liver.txt 0.9 out/res-liver-gradient.png --gradient
+
+echo watershed
+echo bin/iftSegmentObject data/bird.png data/bird.txt 0.7 out/res-bird-watershed.png --watershed
+bin/iftSegmentObject data/bird.png data/bird.txt 0.7 out/res-bird-watershed.png --watershed
+echo bin/iftSegmentObject data/cat.png data/cat.txt 0.8 out/res-cat-watershed.png --watershed
+bin/iftSegmentObject data/cat.png data/cat.txt 0.8 out/res-cat-watershed.png --watershed
+echo bin/iftSegmentObject data/dog.png data/dog.txt 0.7 out/res-dog-watershed.png --watershed
+bin/iftSegmentObject data/dog.png data/dog.txt 0.7 out/res-dog-watershed.png --watershed
+echo bin/iftSegmentObject data/flowers.png data/flowers.txt 0.1 out/res-flowers-watershed.png --watershed
+bin/iftSegmentObject data/flowers.png data/flowers.txt 0.1 out/res-flowers-watershed.png --watershed
+echo bin/iftSegmentObject data/horses.png data/horses.txt 0.9 out/res-horses-watershed.png --watershed
+bin/iftSegmentObject data/horses.png data/horses.txt 0.9 out/res-horses-watershed.png --watershed
+echo bin/iftSegmentObject data/melanoma.png data/melanoma.txt 0.9 out/res-melanoma-watershed.png --watershed
+bin/iftSegmentObject data/melanoma.png data/melanoma.txt 0.9 out/res-melanoma-watershed.png --watershed
+echo bin/iftSegmentObject data/parasite.png data/parasite.txt 0.5 out/res-parasite-watershed.png --watershed
+bin/iftSegmentObject data/parasite.png data/parasite.txt 0.5 out/res-parasite-watershed.png --watershed
+echo bin/iftSegmentObject data/rocks.png data/rocks.txt 0.9 out/res-rocks-watershed.png --watershed
+bin/iftSegmentObject data/rocks.png data/rocks.txt 0.9 out/res-rocks-watershed.png --watershed
+echo bin/iftSegmentObject data/surfer.png data/surfer.txt 0.8 out/res-surfer-watershed.png --watershed
+bin/iftSegmentObject data/surfer.png data/surfer.txt 0.8 out/res-surfer-watershed.png --watershed
+echo bin/iftSegmentObject data/teddy.png data/teddy.txt 0.7 out/res-teddy-watershed.png --watershed
+bin/iftSegmentObject data/teddy.png data/teddy.txt 0.7 out/res-teddy-watershed.png --watershed
+echo bin/iftSegmentObject data/liver.png data/liver.txt 0.9 out/res-liver-watershed.png --watershed
+bin/iftSegmentObject data/liver.png data/liver.txt 0.9 out/res-liver-watershed.png --watershed
+
+echo watershed
+echo bin/iftSegmentObject data/bird.png data/bird.txt 0.7 out/res-bird-oriented.png --oriented-watershed
+bin/iftSegmentObject data/bird.png data/bird.txt 0.7 out/res-bird-oriented.png --oriented-watershed
+echo bin/iftSegmentObject data/cat.png data/cat.txt 0.8 out/res-cat-oriented.png --oriented-watershed
+bin/iftSegmentObject data/cat.png data/cat.txt 0.8 out/res-cat-oriented.png --oriented-watershed
+echo bin/iftSegmentObject data/dog.png data/dog.txt 0.7 out/res-dog-oriented.png --oriented-watershed
+bin/iftSegmentObject data/dog.png data/dog.txt 0.7 out/res-dog-oriented.png --oriented-watershed
+echo bin/iftSegmentObject data/flowers.png data/flowers.txt 0.1 out/res-flowers-oriented.png --oriented-watershed
+bin/iftSegmentObject data/flowers.png data/flowers.txt 0.1 out/res-flowers-oriented.png --oriented-watershed
+echo bin/iftSegmentObject data/horses.png data/horses.txt 0.9 out/res-horses-oriented.png --oriented-watershed
+bin/iftSegmentObject data/horses.png data/horses.txt 0.9 out/res-horses-oriented.png --oriented-watershed
+echo bin/iftSegmentObject data/melanoma.png data/melanoma.txt 0.9 out/res-melanoma-oriented.png --oriented-watershed
+bin/iftSegmentObject data/melanoma.png data/melanoma.txt 0.9 out/res-melanoma-oriented.png --oriented-watershed
+echo bin/iftSegmentObject data/parasite.png data/parasite.txt 0.5 out/res-parasite-oriented.png --oriented-watershed
+bin/iftSegmentObject data/parasite.png data/parasite.txt 0.5 out/res-parasite-oriented.png --oriented-watershed
+echo bin/iftSegmentObject data/rocks.png data/rocks.txt 0.9 out/res-rocks-oriented.png --oriented-watershed
+bin/iftSegmentObject data/rocks.png data/rocks.txt 0.9 out/res-rocks-oriented.png --oriented-watershed
+echo bin/iftSegmentObject data/surfer.png data/surfer.txt 0.8 out/res-surfer-oriented.png --oriented-watershed
+bin/iftSegmentObject data/surfer.png data/surfer.txt 0.8 out/res-surfer-oriented.png --oriented-watershed
+echo bin/iftSegmentObject data/teddy.png data/teddy.txt 0.7 out/res-teddy-oriented.png --oriented-watershed
+bin/iftSegmentObject data/teddy.png data/teddy.txt 0.7 out/res-teddy-oriented.png --oriented-watershed
+echo bin/iftSegmentObject data/liver.png data/liver.txt 0.9 out/res-liver-oriented.png --oriented-watershed
+bin/iftSegmentObject data/liver.png data/liver.txt 0.9 out/res-liver-oriented.png --oriented-watershed
+
+echo Dynamic arc weight
+echo bin/iftSegmentObject data/bird.png data/bird.txt 0.7 out/res-bird-w5.png --dynamic-arc-weight w5
+bin/iftSegmentObject data/bird.png data/bird.txt 0.7 out/res-bird-w5.png --dynamic-arc-weight w5
+echo bin/iftSegmentObject data/cat.png data/cat.txt 0.8 out/res-cat-w5.png --dynamic-arc-weight w5
+bin/iftSegmentObject data/cat.png data/cat.txt 0.8 out/res-cat-w5.png --dynamic-arc-weight w5
+echo bin/iftSegmentObject data/dog.png data/dog.txt 0.7 out/res-dog-w5.png --dynamic-arc-weight w5
+bin/iftSegmentObject data/dog.png data/dog.txt 0.7 out/res-dog-w5.png --dynamic-arc-weight w5
+echo bin/iftSegmentObject data/flowers.png data/flowers.txt 0.1 out/res-flowers-w5.png --dynamic-arc-weight w5
+bin/iftSegmentObject data/flowers.png data/flowers.txt 0.1 out/res-flowers-w5.png --dynamic-arc-weight w5
+echo bin/iftSegmentObject data/horses.png data/horses.txt 0.9 out/res-horses-w5.png --dynamic-arc-weight w5
+bin/iftSegmentObject data/horses.png data/horses.txt 0.9 out/res-horses-w5.png --dynamic-arc-weight w5
+echo bin/iftSegmentObject data/melanoma.png data/melanoma.txt 0.9 out/res-melanoma-w5.png --dynamic-arc-weight w5
+bin/iftSegmentObject data/melanoma.png data/melanoma.txt 0.9 out/res-melanoma-w5.png --dynamic-arc-weight w5
+echo bin/iftSegmentObject data/parasite.png data/parasite.txt 0.5 out/res-parasite-w5.png --dynamic-arc-weight w5
+bin/iftSegmentObject data/parasite.png data/parasite.txt 0.5 out/res-parasite-w5.png --dynamic-arc-weight w5
+echo bin/iftSegmentObject data/rocks.png data/rocks.txt 0.9 out/res-rocks-w5.png --dynamic-arc-weight w5
+bin/iftSegmentObject data/rocks.png data/rocks.txt 0.9 out/res-rocks-w5.png --dynamic-arc-weight w5
+echo bin/iftSegmentObject data/surfer.png data/surfer.txt 0.8 out/res-surfer-w5.png --dynamic-arc-weight w5
+bin/iftSegmentObject data/surfer.png data/surfer.txt 0.8 out/res-surfer-w5.png --dynamic-arc-weight w5
+echo bin/iftSegmentObject data/teddy.png data/teddy.txt 0.7 out/res-teddy-w5.png --dynamic-arc-weight w5
+bin/iftSegmentObject data/teddy.png data/teddy.txt 0.7 out/res-teddy-w5.png --dynamic-arc-weight w5
+echo bin/iftSegmentObject data/liver.png data/liver.txt 0.9 out/res-liver-w5.png --dynamic-arc-weight w5
+bin/iftSegmentObject data/liver.png data/liver.txt 0.9 out/res-liver-w5.png --dynamic-arc-weight w5
+
+echo Dynamic arc weight
+echo bin/iftSegmentObject data/bird.png data/bird.txt 0.7 out/res-bird-w4.png --dynamic-arc-weight w4
+bin/iftSegmentObject data/bird.png data/bird.txt 0.7 out/res-bird-w4.png --dynamic-arc-weight w4
+echo bin/iftSegmentObject data/cat.png data/cat.txt 0.8 out/res-cat-w4.png --dynamic-arc-weight w4
+bin/iftSegmentObject data/cat.png data/cat.txt 0.8 out/res-cat-w4.png --dynamic-arc-weight w4
+echo bin/iftSegmentObject data/dog.png data/dog.txt 0.7 out/res-dog-w4.png --dynamic-arc-weight w4
+bin/iftSegmentObject data/dog.png data/dog.txt 0.7 out/res-dog-w4.png --dynamic-arc-weight w4
+echo bin/iftSegmentObject data/flowers.png data/flowers.txt 0.1 out/res-flowers-w4.png --dynamic-arc-weight w4
+bin/iftSegmentObject data/flowers.png data/flowers.txt 0.1 out/res-flowers-w4.png --dynamic-arc-weight w4
+echo bin/iftSegmentObject data/horses.png data/horses.txt 0.9 out/res-horses-w4.png --dynamic-arc-weight w4
+bin/iftSegmentObject data/horses.png data/horses.txt 0.9 out/res-horses-w4.png --dynamic-arc-weight w4
+echo bin/iftSegmentObject data/melanoma.png data/melanoma.txt 0.9 out/res-melanoma-w4.png --dynamic-arc-weight w4
+bin/iftSegmentObject data/melanoma.png data/melanoma.txt 0.9 out/res-melanoma-w4.png --dynamic-arc-weight w4
+echo bin/iftSegmentObject data/parasite.png data/parasite.txt 0.5 out/res-parasite-w4.png --dynamic-arc-weight w4
+bin/iftSegmentObject data/parasite.png data/parasite.txt 0.5 out/res-parasite-w4.png --dynamic-arc-weight w4
+echo bin/iftSegmentObject data/rocks.png data/rocks.txt 0.9 out/res-rocks-w4.png --dynamic-arc-weight w4
+bin/iftSegmentObject data/rocks.png data/rocks.txt 0.9 out/res-rocks-w4.png --dynamic-arc-weight w4
+echo bin/iftSegmentObject data/surfer.png data/surfer.txt 0.8 out/res-surfer-w4.png --dynamic-arc-weight w4
+bin/iftSegmentObject data/surfer.png data/surfer.txt 0.8 out/res-surfer-w4.png --dynamic-arc-weight w4
+echo bin/iftSegmentObject data/teddy.png data/teddy.txt 0.7 out/res-teddy-w4.png --dynamic-arc-weight w4
+bin/iftSegmentObject data/teddy.png data/teddy.txt 0.7 out/res-teddy-w4.png --dynamic-arc-weight w4
+echo bin/iftSegmentObject data/liver.png data/liver.txt 0.9 out/res-liver-w4.png --dynamic-arc-weight w4
+bin/iftSegmentObject data/liver.png data/liver.txt 0.9 out/res-liver-w4.png --dynamic-arc-weight w4
